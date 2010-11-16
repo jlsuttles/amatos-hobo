19664608c9c8c8978d65028775eed2757700fe34
Jessica Suttles <jlsuttles@gmail.com>
2010-11-16 00:13:20 -0800

    revert some changes

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -3,8 +3,8 @@ class FrontController < ApplicationController
   hobo_controller
 
   def index
-    @about_us = AboutUs.all(:limit => 1)
-    @events = Event.all(:limit => 3, :order => 'date DESC, time DESC')
+    @about_us = AboutUs.all(:limit => 5)
+    @events = Event.all(:limit => 5, :order => 'date DESC, time DESC')
     @locations = Location.all(:limit => 2)
     @store_hours = StoreHours.all(:limit => 5)
     @menus = Menu.all
