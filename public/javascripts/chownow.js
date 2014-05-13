9c7e916a19c922a0b94f2e0fdf65b10a96e73162
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-05-13 13:41:21 -0700

    Update order online button

---
+++ public/javascripts/chownow.js
@@ -0,0 +1,9 @@
+<script>
+  (function(d, s, id) {
+    var js, dt = new Date().getTime(), jst = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://ordering.chownow.com/static/js/iframe.js?'+dt+'#cid=1254';
+    jst.parentNode.insertBefore(js, jst);
+  }(document, 'script', 'chownow-js'));
+</script>
